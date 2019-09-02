90ae6d803cfbd81d3bfcd74f42ef5c6db150eeb9
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2019-09-02 21:45:29 +0530

    u4

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,28 +1,143 @@
-import React from "react";
-import styles from "./AppHeader.css"; // eslint-disable-line
-import { Link } from "react-router-dom";
-// reactstrap components
-import { NavbarBrand, Navbar, Container, Row, Col } from "reactstrap";
+import React, { Component } from "react";
+import {
+  Button,
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  Media
+} from "reactstrap";
 
-const AppHeader = props => {
-  return (
-    <Navbar className={"fixed-top "} color-on-scroll="100" expand="lg">
-      <Container>
-       
 
-        <div className="navbar">
-          <Row>
-            <Col >
-              <a href="#pablo" onClick={e => e.preventDefault()}>
-               REVIT 
-              </a>
-            </Col>
-            <Col className="collapse-close text-right" xs="6"></Col>
-          </Row>
-        </div>
-      </Container>
-    </Navbar>
-  );
-};
+export default class AppHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapseOpen: false,
+      color: "navbar-transparent",
+      left: false
+    };
+  }
+  componentDidMount() {
+    // attach event listeners
+    window.addEventListener("scroll", this.changeColor);
+  }
+  componentWillUnmount() {
+    // remove event listeners
+    window.removeEventListener("scroll", this.changeColor);
+  }
 
-export default AppHeader;
+  changeColor = () => {
+    if (
+      document.documentElement.scrollTop > 99 ||
+      document.body.scrollTop > 99
+    ) {
+      this.setState({
+        color: "bg-info"
+      });
+    } else if (
+      document.documentElement.scrollTop < 100 ||
+      document.body.scrollTop < 100
+    ) {
+      this.setState({
+        color: "navbar-transparent"
+      });
+    }
+  };
+
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
+  handleChange = event => {
+    this.setState({ auth: event.target.checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  render() {
+    const { showMenu } = this.props;
+    const showOrHide = showMenu ? "flex" : "none";
+    return (
+      <Navbar
+        className={"fixed-top " + this.state.color}
+        color-on-scroll="100"
+        expand="lg"
+      >
+        <Container>
+          <div className="navbar-translate">
+            <NavbarBrand
+              data-placement="bottom"
+              to="/"
+              rel="noopener noreferrer"
+              title=""
+            >
+              <span>REVIT </span>
+            </NavbarBrand>
+            <button
+              aria-expanded={this.state.collapseOpen}
+              className="navbar-toggler navbar-toggler"
+              onClick={this.toggleCollapse}
+            >
+              <span className="navbar-toggler-bar bar1" />
+              <span className="navbar-toggler-bar bar2" />
+              <span className="navbar-toggler-bar bar3" />
+            </button>
+          </div>
+          <Collapse
+            className={"justify-content-end " + this.state.collapseOut}
+            navbar
+            isOpen={this.state.collapseOpen}
+            onExiting={this.onCollapseExiting}
+            onExited={this.onCollapseExited}
+          >
+            <div className="navbar-collapse-header">
+              <Row>
+                <Col className="collapse-brand" xs="6">
+                  <a href="#pablo" onClick={e => e.preventDefault()}>
+                    REVIT
+                  </a>
+                </Col>
+                <Col className="collapse-close text-right" xs="6">
+                  <button
+                    aria-expanded={this.state.collapseOpen}
+                    className="navbar-toggler"
+                    onClick={this.toggleCollapse}
+                  >
+                    <i className="tim-icons icon-simple-remove" />
+                  </button>
+                </Col>
+              </Row>
+            </div>
+            <Nav navbar>
+              <NavItem className="p-0">
+                <NavLink href="/">
+                  <p className="">Home</p>
+                </NavLink>
+                <NavLink href="/students">
+                  <p className="">Students</p>
+                </NavLink>
+              </NavItem>
+              <NavItem className="p-0">
+                <NavLink href="/staffs">
+                  <p className="">Staffs</p>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}

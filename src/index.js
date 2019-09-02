3201c8d505adb1a0ebd0b0dfabe2f885a9a7d6d1
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2019-09-02 16:24:29 +0530

    u1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/blk-design-system-react.css";
+import "./assets/demo/demo.css";
 import './index.css'
 import App from './App'
 

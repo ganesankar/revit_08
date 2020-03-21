bd0b6590cb5c2e5518cd78bf0a90d0fed657b989
Ganesan Karuppaiya <ganesan.intech@gmail.com>
2020-03-22 01:22:15 +0530

    1

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -24,32 +24,29 @@ export default class AppHeader extends Component {
       userlogged: {},
       color: "navbar-transparent",
       left: false,
-      sessionUser : window.sessionStorage.getItem('revitGoogleID') || '' 
-
+      sessionUser: window.sessionStorage.getItem("revitGoogleID") || ""
     };
   }
   componentDidMount() {
     // attach event listeners
     // window.sessionStorage.setItem("revitGoogleID", googleData.googleID);
     //window.sessionStorage.getItem('revitUserID')
-    const alreadylogged = window.sessionStorage.getItem('revitGoogleID') || '' ;
-    
-   
-    
+    const alreadylogged = window.sessionStorage.getItem("revitGoogleID") || "";
+
     window.addEventListener("scroll", this.changeColor);
   }
   componentWillUnmount() {
     // remove event listeners
     window.removeEventListener("scroll", this.changeColor);
   }
-  
-  getrecordId = (todo) => {
+
+  getrecordId = todo => {
     if (!todo.ref) {
       return null;
     }
     return todo.ref["@ref"].id;
   };
-  updateUserLogin = (data) => {
+  updateUserLogin = data => {
     this.setState({
       userlogin: true,
       userlogged: data
@@ -91,28 +88,28 @@ export default class AppHeader extends Component {
       lastname: response.profileObj.familyName,
       avatar: response.profileObj.imageUrl,
       accesstoken: response.accessToken,
-      lastlogin : new Date().getTime() * 10000,
-      total:0
+      lastlogin: new Date().getTime() * 10000,
+      total: 0
     };
     this.setState({
       userlogin: true,
       userlogged: googleData
     });
-   
+
     api
-        .createSession(googleData)
-        .then(response => {
-          const returnId = this.getrecordId(response);
-          if(returnId){
-            window.sessionStorage.setItem("revituserID", returnId);
-            window.sessionStorage.setItem("revitGoogleID", googleData.googleID);
-          }
-          ToastsStore.success(`User Login Updated!`);
-        })
-        .catch(e => {
-          console.log("An API error occurred", e);
-          ToastsStore.error(`User Login Update Failed!`);
-        });
+      .createSession(googleData)
+      .then(response => {
+        const returnId = this.getrecordId(response);
+        if (returnId) {
+          window.sessionStorage.setItem("revituserID", returnId);
+          window.sessionStorage.setItem("revitGoogleID", googleData.googleID);
+        }
+        ToastsStore.success(`User Login Updated!`);
+      })
+      .catch(e => {
+        console.log("An API error occurred", e);
+        ToastsStore.error(`User Login Update Failed!`);
+      });
     console.log(googleData);
   };
 
@@ -175,57 +172,6 @@ export default class AppHeader extends Component {
                 </Col>
               </Row>
             </div>
-            <Nav navbar>
-              <NavItem className="p-0">
-                <NavLink href="/">
-                  <p className="">Home</p>
-                </NavLink>
-              </NavItem>{" "}
-              <NavItem className="p-0">
-                <NavLink href="/students">
-                  <p className="">Students</p>
-                </NavLink>
-              </NavItem>
-              <NavItem className="p-0">
-                <NavLink href="/staffs">
-                  <p className="">Staffs</p>
-                </NavLink>
-              </NavItem>
-              <NavItem className="p-0">
-                <NavLink href="/calendar">
-                  <p className="">Events</p>
-                </NavLink>
-              </NavItem>
-              <NavItem className="p-0">
-                <NavLink href="/articles">
-                  <p className="">Posts</p>
-                </NavLink>
-              </NavItem>
-              <NavItem className="p-0">
-                {this.state.userlogin ? (
-                  <GoogleLogout
-                    clientId="157852765565-21eh7v2tvqv5r7t8fg28o6073kqt3so3.apps.googleusercontent.com"
-                    buttonText="Logout"
-                    onLogoutSuccess={this.logout}
-                    disabledStyle={true}
-                    className="btn-neutral btn btn-default logoutBtn"
-                  >
-                    {" "}
-                    {this.state.userlogged.username} | Logout
-                  </GoogleLogout>
-                ) : (
-                  <GoogleLogin
-                    clientId="157852765565-21eh7v2tvqv5r7t8fg28o6073kqt3so3.apps.googleusercontent.com"
-                    buttonText="Login"
-                    className="btn-neutral btn btn-default logoutBtn"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                    disabledStyle={true}
-                    cookiePolicy={"single_host_origin"}
-                  />
-                )}
-              </NavItem>
-            </Nav>
           </Collapse>
           <div className="float-right"></div>
         </Container>
--- src/views/Home.js
+++ src/views/Home.js
@@ -11,44 +11,24 @@ export default class Home extends Component {
     // Fetch all todos
   }
 
-  
-
   render() {
     return (
       <div className="app">
-        <Container>
-          <div className="content-center brand">
-            <h1 className="h1-seo">REVIT 2008</h1>
-            <h3 className="d-none d-sm-block">JJCET 2008 IT BATCH COMMUNITY</h3>
-            
-            <Nav className="nav-pills-info nav-pills-icons" pills>
-              <NavItem>
-                <NavLink className="nav-link" to="/students">
-                  <i className="tim-icons icon-atom" />
-                  Students
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link" to="/staffs">
-                  <i className="tim-icons icon-chat-33" />
-                  Staffs
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/events" className="nav-link">
-                  <i className="tim-icons icon-settings-gear-63" />
-                  Events
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/links" className="nav-link">
-                  <i className="tim-icons icon-settings-gear-63" />
-                  Links
-                </NavLink>
-              </NavItem>
-            </Nav>
+        <div className="full-page lock-page">
+          <div className="content">
+            <div className="container">
+              <div className="ml-auto mr-auto col-md-6 col-lg-4">
+                <div className="card-lock card-white text-center card">
+                  <div className="card-header"></div>
+                  <div className="card-body">
+                    <h4 className="card-title"> Access Restriceted !</h4>
+                  </div>
+                  <div className="card-footer"></div>
+                </div>
+              </div>
+            </div>
           </div>
-        </Container>
+        </div>
       </div>
     );
   }
